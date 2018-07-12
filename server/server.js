a94eff2c17bdc7b0ed0aaca6db728538699c9f34
ebikila <boleighaebi4real@gmail.com>
2018-07-12 08:06:08 -0700

    Greet new user, and alart others

--- server/server.js
+++ server/server.js
@@ -28,6 +28,19 @@ io.on('connection',(socket) => {
 //socket.on('createEmail', (newEmail) => {
     //console.log('createEmail', newEmail);
 //});
+
+socket.emit('newMessage', {
+    from:'Admin',
+    text: 'Welcome to the chat app',
+    createAt:new Date().getTime()
+});
+
+socket.broadcast.emit('newMessage', {
+    from:'Admin',
+    text:'New user joined',
+    createAt:new Date().getTime()
+});
+
 socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', {
@@ -35,6 +48,11 @@ from:message.from,
 text:message.text,
 createAt:new Date().getTime()
     });
+  // socket.broadcast.emit('newMessage', {
+       //from:message.from,
+       //text:message.text,
+       //createAt: new Date().getTime()
+   //});
 });
 
     socket.on('disconnect', () => {
